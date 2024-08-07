bae60622dc5dab8a5d3b86fef20c96503418c1e2
Abdelrahman Samy <abdelrahmansamy123>
2024-08-07 22:41:03 +0300

    create home theme for splash screen

--- app/src/main/java/com/training/ecommerce/MainActivity.kt
+++ app/src/main/java/com/training/ecommerce/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.training.ecommerce
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        initialSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initialSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+            //Add a callback that's called when the splash screen is animating to the
+            //app content
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+
+                //create your custom animation
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 2000L
+
+                //call splashScreenView.remove at the end of your custom animation
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                //Run your animation
+                slideUp.start()
+
+            }
+
+        } else {
+            setTheme(R.style.Theme_ECommerce)
+        }
     }
 }
